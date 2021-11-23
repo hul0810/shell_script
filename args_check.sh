5383c8c3fd693e0946337e7bf6d80884ff11f6ff
hul0810 <67278262+hul0810@users.noreply.github.com>
2021-11-23 23:55:19 +0900

    전달 인자 체크

---
+++ args_check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "usage: $0 directory"
+  exit
+fi
+
+echo 'Have a nice day!'
+
+---
+
+# 설명 
+# -z STRING: 문자열 비어 있으면 True(길이가 0임)
+
+---
+
+# 내가 작성한 스크립트
+#!/bin/bash
+
+#if [[ $# -eq 0 ]]; then
+#  echo 'usage: ./goodday.sh directory'
+#else
+#  echo 'Have a nice day!'
+#fi
